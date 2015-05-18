538b9424f6aff9ecfad78fe9d6aadbf07db8bd6d
BlackEdder <edder@tkwsping.nl>
2015-05-18 14:07:23 +0100

    Added license file and reference to the paper in the README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 
+
+## flu-evidence
+
+This repository contains the code used for flu data analysis first published in
+
+Baguelin, Marc, Stefan Flasche, Anton Camacho, Nikolaos Demiris, Elizabeth Miller, and W. John Edmunds. ‘Assessing Optimal Target Populations for Influenza Vaccination Programmes: An Evidence Synthesis and Modelling Study’. PLoS Med 10, no. 10 (8 October 2013): e1001527. doi:10.1371/journal.pmed.1001527.
+
+The code is distributed under a GPLv3 license. Please remember to cite the above manuscript when using this code.
+
+
 ## INSTALL
 
 To successfully install this you need cmake, gsl and boost installed. Then run:
--- src/inference.cc
+++ src/inference.cc
@@ -2,6 +2,7 @@
 #include "boost/filesystem.hpp"
 #include "boost/regex.hpp"
 
+#include "model.hh"
 #include "state.hh"
 
 using namespace flu;
@@ -61,10 +62,66 @@ int main(int argc, char *argv[])
 
     sort( ks.begin(), ks.end() );
 
+
+
+    FILE *Scen1FS, *Scen2FS;
+    Scen1FS=write_file(data_path + "scenarii/Scenario_vaccination_final_size.txt");
+
+    /*opens the 2nd scenarioFile*/
+    Scen2FS=write_file(data_path + "scenarii/Scenario_no_vaccination_final_size.txt");
+
+    /*load the size of the age groups for the model that year*/
+    FILE *f_pop_model;
+    double pop_vec[21];
+    save_fscanf(f_pop_model,"%lf %lf %lf %lf %lf %lf %lf",&pop_vec[0],&pop_vec[1],&pop_vec[2],&pop_vec[3],&pop_vec[4],&pop_vec[5],&pop_vec[6]);
+    /*high risk*/
+    pop_vec[7]=pop_vec[0]*0.021; /*2.1% in the <1 */
+    pop_vec[8]=pop_vec[1]*0.055; /*5.5% in the 1-4 */
+    pop_vec[9]=pop_vec[2]*0.098; /*9.8% in the 5-14 */
+    pop_vec[10]=pop_vec[3]*0.087; /*8.7% in the 15-24 */
+    pop_vec[11]=pop_vec[4]*0.092; /*9.2% in the 25-44 */
+    pop_vec[12]=pop_vec[5]*0.183; /*18.3% in the 45-64 */
+    pop_vec[13]=pop_vec[6]*0.45; /*45% in the 65+ */
+
+    /*pregnant women (pregnant women not considered in this model*/
+    pop_vec[14]=0;
+    pop_vec[15]=0;
+    pop_vec[16]=0;
+    pop_vec[17]=0;
+    pop_vec[18]=0;
+    pop_vec[19]=0;
+    pop_vec[20]=0;
+
+    /*low risk*/
+    pop_vec[0]-=pop_vec[7];
+    pop_vec[1]-=pop_vec[8];
+    pop_vec[2]-=pop_vec[9];
+    pop_vec[3]-=pop_vec[10]+pop_vec[17];
+    pop_vec[4]-=pop_vec[11]+pop_vec[18];
+    pop_vec[5]-=pop_vec[12];
+    pop_vec[6]-=pop_vec[13];
+    fclose( f_pop_model );
+
+    double curr_init_inf[NAG];
+    //double current_contact_regular[NAG2];
+    
+    //int n_scenarii;
+
     for( auto & k : ks ) 
     {
+        auto state = load_state( data_path + "samples/z_hyper" 
+                + boost::lexical_cast<std::string>( k )
+                + ".stm", NAG, POLY_PART );
+
+        /*translate into an initial infected population*/
+        for(size_t i=0;i<NAG;i++)
+            curr_init_inf[i]=pow(10,state.parameters.init_pop);
+
+        //save_scenarii(Scen1FS, Scen2FS, pop_vec, curr_init_inf, state, current_contact_regular, n_scenarii, tab_cal, tab_VE, data_path, &First_write);
         std::cout << k << std::endl;
     }
+    fclose(Scen1FS);
+    fclose(Scen2FS);
 
     return 0;
 }
