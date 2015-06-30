fbb9d37493ead9e9dc6a4952d4a403197d379243
BlackEdder <edder@tkwsping.nl>
2015-06-30 17:03:51 +0100

    Ignore object files with autotest.sh

--- fluEvidenceSynthesis/autotest.sh
+++ fluEvidenceSynthesis/autotest.sh
@@ -7,4 +7,4 @@ echo -e "#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)" > src/RcppExports
 R CMD check ../fluEvidenceSynthesis
 
 # This script depends on inotify-hookable
-inotify-hookable -q -w data -w src -w tests -f DESCRIPTION -c "R -e 'Rcpp::compileAttributes(\".\",verbose=TRUE)' && echo '#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)' > src/RcppExports.cpp && R CMD check ../fluEvidenceSynthesis; sleep 10"
+inotify-hookable -t 1000 -i '*.o' -i '\..*' -w data -w src -w tests -f DESCRIPTION -c "R -e 'Rcpp::compileAttributes(\".\",verbose=TRUE)' && echo '#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)' > src/RcppExports.cpp && R CMD check ../fluEvidenceSynthesis; sleep 10"
