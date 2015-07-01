10732854f500ea4e55d3e74b32c0d05839cf95b8
BlackEdder <edder@tkwsping.nl>
2015-07-01 10:58:20 +0100

    Work on better R functions

--- fluEvidenceSynthesis/R/vaccine.R
+++ fluEvidenceSynthesis/R/vaccine.R
@@ -14,6 +14,33 @@ load.data<-function()
   save(cov.figures,uptake.figures,year.match,file=paste(HOME,"Rsave/vaccine_env.R",sep="/"))
 }
  
+vaccine.calendar <- function( coverage, efficacy, uptake )
+{
+    new.vacc.cal<-matrix(rep(0,123*21),ncol=21)
+
+    #load the coverage figures
+    #cov.array.year.scen[(i-1)*(n_scenario+2)+n_s,]<-c(season.fnames[i],n_s,LR.cov/100,HR.cov/100)
+
+    LR.cov<-as.numeric(coverage[1:7])
+    HR.cov<-as.numeric(coverage[8:14])
+
+    #Octobre
+    for(j in 1:31)
+        new.vacc.cal[j,]<-c(LR.cov,HR.cov,LR.cov)*c(rep(uptake[2],6),uptake[3],rep(uptake[2],6),uptake[3],rep(uptake[2],6),uptake[3])/(31*100)  
+    #Novembre
+    for(j in 32:61)
+        new.vacc.cal[j,]<-c(LR.cov,HR.cov,LR.cov)*c(rep(uptake[4]-uptake[2],6),uptake[5]-uptake[3],rep(uptake[4]-uptake[2],6),uptake[5]-uptake[3],rep(uptake[4]-uptake[2],6),uptake[5]-uptake[3])/(30*100)
+    #Decembre
+    for(j in 62:92)
+        new.vacc.cal[j,]<-c(LR.cov,HR.cov,LR.cov)*c(rep(uptake[6]-uptake[4],6),uptake[7]-uptake[5],rep(uptake[6]-uptake[4],6),uptake[7]-uptake[5],rep(uptake[6]-uptake[4],6),uptake[7]-uptake[5])/(31*100)
+    #Janvier
+    for(j in 93:123)
+        new.vacc.cal[j,]<-c(LR.cov,HR.cov,LR.cov)*c(rep(100-uptake[6],6),100-uptake[7],rep(100-uptake[6],6),100-uptake[7],rep(100-uptake[6],6),100-uptake[7])/(31*100)
+
+    cal <- list( "efficacy"=efficacy, "calendar"=new.vacc.cal )
+    cal
+}
+
 generate.vaccine.calendar.file<-function(coverage.scenarios, efficacy,name.file="vaccine_calendar.txt")
 {
     year <- 199900
