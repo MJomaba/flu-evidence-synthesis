41d1ab978a4387573e971d225c0793dd96772029
BlackEdder <edder@tkwsping.nl>
2015-07-01 16:39:31 +0100

    Added proper test

--- fluEvidenceSynthesis/tests/testthat/testVaccine.R
+++ fluEvidenceSynthesis/tests/testthat/testVaccine.R
@@ -12,7 +12,7 @@ test_that("We can load contact data",
               data("polymod_uk")
               expect_that( nrow(polymod_uk), equals(597) )
               expect_that( ncol(polymod_uk), equals(9) )
-              expect_true( polymod_uk$weekend[2] )
+              expect_that( polymod_uk[2,2], equals(1) )
           }
 )
 
@@ -40,12 +40,14 @@ test_that("We can call scenario",
               data("vaccine_calendar")
               data("mcmcsample")
               data("polymod_uk")
-              expect_that( length(
-                vaccinationScenario( age_sizes=age_sizes[,1], 
+
+              total_size <- vaccinationScenario( age_sizes=age_sizes[,1], 
                     vaccine_calendar=vaccine_calendar,
                     sample=mcmcsample,
-                    polymod_uk=as.matrix(polymod_uk) ) ),
-                                  equals(21)
-                )
+                    polymod_uk=as.matrix(polymod_uk) )
+
+              expect_equal( total_size,
+                    c( 103841.212, 560157.389, 1284767.061, 2607491.160, 3884830.785, 2428974.589, 328566.100, 2181.114, 28518.728, 118383.065, 168843.285, 264320.534, 380895.545, 268826.809, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.000 )
+                               )
           }
 )
