2a7446bde62ec560574de0a2aadb3430e8610dc2
BlackEdder <edder@tkwsping.nl>
2015-07-15 12:18:44 +0100

    Use more std::map<group_types_t,seir_t> for deltas

--- fluEvidenceSynthesis/src/model.cc
+++ fluEvidenceSynthesis/src/model.cc
@@ -58,15 +58,10 @@ namespace flu
         double S[7], E1[7], E2[7], I1[7], I2[7], R[7];
         double Sr[7], E1r[7], E2r[7], I1r[7], I2r[7], Rr[7];
         double Sp[7], E1p[7], E2p[7], I1p[7], I2p[7], Rp[7];
-        double deltaS[7], deltaE1[7], deltaE2[7], deltaI1[7], deltaI2[7], deltaR[7];
-        double deltaSr[7], deltaE1r[7], deltaE2r[7], deltaI1r[7], deltaI2r[7], deltaRr[7];
-        double deltaSp[7], deltaE1p[7], deltaE2p[7], deltaI1p[7], deltaI2p[7], deltaRp[7];
+
         double SN[7], E1N[7], E2N[7], I1N[7], I2N[7], RN[7];
         double SrN[7], E1rN[7], E2rN[7], I1rN[7], I2rN[7], RrN[7];
         double SpN[7], E1pN[7], E2pN[7], I1pN[7], I2pN[7], RpN[7];
-        double deltaSN[7], deltaE1N[7], deltaE2N[7], deltaI1N[7], deltaI2N[7], deltaRN[7];
-        double deltaSrN[7], deltaE1rN[7], deltaE2rN[7], deltaI1rN[7], deltaI2rN[7], deltaRrN[7];
-        double deltaSpN[7], deltaE1pN[7], deltaE2pN[7], deltaI1pN[7], deltaI2pN[7], deltaRpN[7];
         double total_of_new_cases_per_day[7], total_of_new_cases_per_day_r[7], total_of_new_cases_per_day_p[7];
         double vacc_prov, vacc_prov_p, vacc_prov_r;
         int i, j;
@@ -156,104 +151,104 @@ namespace flu
                 // accessed via one big vector? (age, risk, seeiir, vaccinated)
                 // Using some simple regexs we could make them into maps, with "S/E1/E2/I1/I2/R", "l/r/p", "N/V" as mappings
                 /*initialisation of the different increment for the euler algorithm*/
-                deltaS[i]=0;
-                deltaE1[i]=0;
-                deltaE2[i]=0;
-                deltaI1[i]=0;
-                deltaI2[i]=0;
-                deltaR[i]=0;
+                deltas[VACC_LOW].s[i]=0;
+                deltas[VACC_LOW].e1[i]=0;
+                deltas[VACC_LOW].e2[i]=0;
+                deltas[VACC_LOW].i1[i]=0;
+                deltas[VACC_LOW].i2[i]=0;
+                deltas[VACC_LOW].r[i]=0;
 
-                deltaSr[i]=0;
-                deltaE1r[i]=0;
-                deltaE2r[i]=0;
-                deltaI1r[i]=0;
-                deltaI2r[i]=0;
-                deltaRr[i]=0;
+                deltas[VACC_HIGH].s[i]=0;
+                deltas[VACC_HIGH].e1[i]=0;
+                deltas[VACC_HIGH].e2[i]=0;
+                deltas[VACC_HIGH].i1[i]=0;
+                deltas[VACC_HIGH].i2[i]=0;
+                deltas[VACC_HIGH].r[i]=0;
 
-                deltaSp[i]=0;
-                deltaE1p[i]=0;
-                deltaE2p[i]=0;
-                deltaI1p[i]=0;
-                deltaI2p[i]=0;
-                deltaRp[i]=0;
+                deltas[VACC_PREG].s[i]=0;
+                deltas[VACC_PREG].e1[i]=0;
+                deltas[VACC_PREG].e2[i]=0;
+                deltas[VACC_PREG].i1[i]=0;
+                deltas[VACC_PREG].i2[i]=0;
+                deltas[VACC_PREG].r[i]=0;
 
-                deltaSN[i]=0;
-                deltaE1N[i]=0;
-                deltaE2N[i]=0;
-                deltaI1N[i]=0;
-                deltaI2N[i]=0;
-                deltaRN[i]=0;
+                deltas[LOW].s[i]=0;
+                deltas[LOW].e1[i]=0;
+                deltas[LOW].e2[i]=0;
+                deltas[LOW].i1[i]=0;
+                deltas[LOW].i2[i]=0;
+                deltas[LOW].r[i]=0;
 
-                deltaSrN[i]=0;
-                deltaE1rN[i]=0;
-                deltaE2rN[i]=0;
-                deltaI1rN[i]=0;
-                deltaI2rN[i]=0;
-                deltaRrN[i]=0;
+                deltas[HIGH].s[i]=0;
+                deltas[HIGH].e1[i]=0;
+                deltas[HIGH].e2[i]=0;
+                deltas[HIGH].i1[i]=0;
+                deltas[HIGH].i2[i]=0;
+                deltas[HIGH].r[i]=0;
 
-                deltaSpN[i]=0;
-                deltaE1pN[i]=0;
-                deltaE2pN[i]=0;
-                deltaI1pN[i]=0;
-                deltaI2pN[i]=0;
-                deltaRpN[i]=0;
+                deltas[PREG].s[i]=0;
+                deltas[PREG].e1[i]=0;
+                deltas[PREG].e2[i]=0;
+                deltas[PREG].i1[i]=0;
+                deltas[PREG].i2[i]=0;
+                deltas[PREG].r[i]=0;
 
                 /*rate of depletion of susceptible*/
                 for(j=0;j<NAG;j++)
-                    deltaS[i]+=transmission_regular(i,j)*(I1[j]+I2[j]+I1r[j]+I2r[j]+I1p[j]+I2p[j]+I1N[j]+I2N[j]+I1rN[j]+I2rN[j]+I1pN[j]+I2pN[j]);
+                    deltas[VACC_LOW].s[i]+=transmission_regular(i,j)*(I1[j]+I2[j]+I1r[j]+I2r[j]+I1p[j]+I2p[j]+I1N[j]+I2N[j]+I1rN[j]+I2rN[j]+I1pN[j]+I2pN[j]);
 
-                deltaSr[i]=deltaS[i];
-                deltaSp[i]=deltaS[i];
-                deltaSN[i]=deltaS[i];
-                deltaSrN[i]=deltaS[i];
-                deltaSpN[i]=deltaS[i];
-                deltaS[i]*=-S[i];
-                deltaSr[i]*=-Sr[i];
-                deltaSp[i]*=-Sp[i];
-                deltaSN[i]*=-SN[i];
-                deltaSrN[i]*=-SrN[i];
-                deltaSpN[i]*=-SpN[i];
+                deltas[VACC_HIGH].s[i]=deltas[VACC_LOW].s[i];
+                deltas[VACC_PREG].s[i]=deltas[VACC_LOW].s[i];
+                deltas[LOW].s[i]=deltas[VACC_LOW].s[i];
+                deltas[HIGH].s[i]=deltas[VACC_LOW].s[i];
+                deltas[PREG].s[i]=deltas[VACC_LOW].s[i];
+                deltas[VACC_LOW].s[i]*=-S[i];
+                deltas[VACC_HIGH].s[i]*=-Sr[i];
+                deltas[VACC_PREG].s[i]*=-Sp[i];
+                deltas[LOW].s[i]*=-SN[i];
+                deltas[HIGH].s[i]*=-SrN[i];
+                deltas[PREG].s[i]*=-SpN[i];
 
                 /*rate of passing between states of infection*/
-                deltaE1[i]=-deltaS[i]-a1*E1[i];
-                deltaE1r[i]=-deltaSr[i]-a1*E1r[i];
-                deltaE1p[i]=-deltaSp[i]-a1*E1p[i];
+                deltas[VACC_LOW].e1[i]=-deltas[VACC_LOW].s[i]-a1*E1[i];
+                deltas[VACC_HIGH].e1[i]=-deltas[VACC_HIGH].s[i]-a1*E1r[i];
+                deltas[VACC_PREG].e1[i]=-deltas[VACC_PREG].s[i]-a1*E1p[i];
 
-                deltaE1N[i]=-deltaSN[i]-a1*E1N[i];
-                deltaE1rN[i]=-deltaSrN[i]-a1*E1rN[i];
-                deltaE1pN[i]=-deltaSpN[i]-a1*E1pN[i];
+                deltas[LOW].e1[i]=-deltas[LOW].s[i]-a1*E1N[i];
+                deltas[HIGH].e1[i]=-deltas[HIGH].s[i]-a1*E1rN[i];
+                deltas[PREG].e1[i]=-deltas[PREG].s[i]-a1*E1pN[i];
 
-                deltaE2[i]=a1*E1[i]-a2*E2[i];
-                deltaE2r[i]=a1*E1r[i]-a2*E2r[i];
-                deltaE2p[i]=a1*E1p[i]-a2*E2p[i];
+                deltas[VACC_LOW].e2[i]=a1*E1[i]-a2*E2[i];
+                deltas[VACC_HIGH].e2[i]=a1*E1r[i]-a2*E2r[i];
+                deltas[VACC_PREG].e2[i]=a1*E1p[i]-a2*E2p[i];
 
-                deltaE2N[i]=a1*E1N[i]-a2*E2N[i];
-                deltaE2rN[i]=a1*E1rN[i]-a2*E2rN[i];
-                deltaE2pN[i]=a1*E1pN[i]-a2*E2pN[i];
+                deltas[LOW].e2[i]=a1*E1N[i]-a2*E2N[i];
+                deltas[HIGH].e2[i]=a1*E1rN[i]-a2*E2rN[i];
+                deltas[PREG].e2[i]=a1*E1pN[i]-a2*E2pN[i];
 
-                deltaI1[i]=a2*E2[i]-g1*I1[i];
-                deltaI1r[i]=a2*E2r[i]-g1*I1r[i];
-                deltaI1p[i]=a2*E2p[i]-g1*I1p[i];
+                deltas[VACC_LOW].i1[i]=a2*E2[i]-g1*I1[i];
+                deltas[VACC_HIGH].i1[i]=a2*E2r[i]-g1*I1r[i];
+                deltas[VACC_PREG].i1[i]=a2*E2p[i]-g1*I1p[i];
 
-                deltaI1N[i]=a2*E2N[i]-g1*I1N[i];
-                deltaI1rN[i]=a2*E2rN[i]-g1*I1rN[i];
-                deltaI1pN[i]=a2*E2pN[i]-g1*I1pN[i];
+                deltas[LOW].i1[i]=a2*E2N[i]-g1*I1N[i];
+                deltas[HIGH].i1[i]=a2*E2rN[i]-g1*I1rN[i];
+                deltas[PREG].i1[i]=a2*E2pN[i]-g1*I1pN[i];
 
-                deltaI2[i]=g1*I1[i]-g2*I2[i];
-                deltaI2r[i]=g1*I1r[i]-g2*I2r[i];
-                deltaI2p[i]=g1*I1p[i]-g2*I2p[i];
+                deltas[VACC_LOW].i2[i]=g1*I1[i]-g2*I2[i];
+                deltas[VACC_HIGH].i2[i]=g1*I1r[i]-g2*I2r[i];
+                deltas[VACC_PREG].i2[i]=g1*I1p[i]-g2*I2p[i];
 
-                deltaI2N[i]=g1*I1N[i]-g2*I2N[i];
-                deltaI2rN[i]=g1*I1rN[i]-g2*I2rN[i];
-                deltaI2pN[i]=g1*I1pN[i]-g2*I2pN[i];
+                deltas[LOW].i2[i]=g1*I1N[i]-g2*I2N[i];
+                deltas[HIGH].i2[i]=g1*I1rN[i]-g2*I2rN[i];
+                deltas[PREG].i2[i]=g1*I1pN[i]-g2*I2pN[i];
 
-                deltaR[i]=g2*I2[i];
-                deltaRr[i]=g2*I2r[i];
-                deltaRp[i]=g2*I2p[i];
+                deltas[VACC_LOW].r[i]=g2*I2[i];
+                deltas[VACC_HIGH].r[i]=g2*I2r[i];
+                deltas[VACC_PREG].r[i]=g2*I2p[i];
 
-                deltaRN[i]=g2*I2N[i];
-                deltaRrN[i]=g2*I2rN[i];
-                deltaRpN[i]=g2*I2pN[i];
+                deltas[LOW].r[i]=g2*I2N[i];
+                deltas[HIGH].r[i]=g2*I2rN[i];
+                deltas[PREG].r[i]=g2*I2pN[i];
             }
 
             /*Vaccine bit*/
@@ -280,100 +275,100 @@ namespace flu
                     vacc_prov_r=Npop[i+NAG]*vaccine_programme.calendar(date_id,i+NAG)/(SrN[i]+E1rN[i]+E2rN[i]+I1rN[i]+I2rN[i]+RrN[i]);
                     vacc_prov_p=0; /*Npop[i+2*NAG]*vaccination_calendar[cal_time*21+i+2*NAG]/(SpN[i]+E1pN[i]+E2pN[i]+I1pN[i]+I2pN[i]+RpN[i]);*/
 
-                    deltaS[i]+=SN[i]*vacc_prov*(1-vaccine_programme.efficacy_year[i]);
-                    deltaSr[i]+=SrN[i]*vacc_prov_r*(1-vaccine_programme.efficacy_year[i]);
-                    deltaSp[i]+=SpN[i]*vacc_prov_p*(1-vaccine_programme.efficacy_year[i]);
-                    deltaSN[i]-=SN[i]*vacc_prov;
-                    deltaSrN[i]-=SrN[i]*vacc_prov_r;
-                    deltaSpN[i]-=SpN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].s[i]+=SN[i]*vacc_prov*(1-vaccine_programme.efficacy_year[i]);
+                    deltas[VACC_HIGH].s[i]+=SrN[i]*vacc_prov_r*(1-vaccine_programme.efficacy_year[i]);
+                    deltas[VACC_PREG].s[i]+=SpN[i]*vacc_prov_p*(1-vaccine_programme.efficacy_year[i]);
+                    deltas[LOW].s[i]-=SN[i]*vacc_prov;
+                    deltas[HIGH].s[i]-=SrN[i]*vacc_prov_r;
+                    deltas[PREG].s[i]-=SpN[i]*vacc_prov_p;
 
-                    deltaE1[i]+=E1N[i]*vacc_prov;
-                    deltaE1r[i]+=E1rN[i]*vacc_prov_r;
-                    deltaE1p[i]+=E1pN[i]*vacc_prov_p;
-                    deltaE1N[i]-=E1N[i]*vacc_prov;
-                    deltaE1rN[i]-=E1rN[i]*vacc_prov_r;
-                    deltaE1pN[i]-=E1pN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].e1[i]+=E1N[i]*vacc_prov;
+                    deltas[VACC_HIGH].e1[i]+=E1rN[i]*vacc_prov_r;
+                    deltas[VACC_PREG].e1[i]+=E1pN[i]*vacc_prov_p;
+                    deltas[LOW].e1[i]-=E1N[i]*vacc_prov;
+                    deltas[HIGH].e1[i]-=E1rN[i]*vacc_prov_r;
+                    deltas[PREG].e1[i]-=E1pN[i]*vacc_prov_p;
 
-                    deltaE2[i]+=E2N[i]*vacc_prov;
-                    deltaE2r[i]+=E2rN[i]*vacc_prov_r;
-                    deltaE2p[i]+=E2pN[i]*vacc_prov_p;
-                    deltaE2N[i]-=E2N[i]*vacc_prov;
-                    deltaE2rN[i]-=E2rN[i]*vacc_prov_r;
-                    deltaE2pN[i]-=E2pN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].e2[i]+=E2N[i]*vacc_prov;
+                    deltas[VACC_HIGH].e2[i]+=E2rN[i]*vacc_prov_r;
+                    deltas[VACC_PREG].e2[i]+=E2pN[i]*vacc_prov_p;
+                    deltas[LOW].e2[i]-=E2N[i]*vacc_prov;
+                    deltas[HIGH].e2[i]-=E2rN[i]*vacc_prov_r;
+                    deltas[PREG].e2[i]-=E2pN[i]*vacc_prov_p;
 
-                    deltaI1[i]+=I1N[i]*vacc_prov;
-                    deltaI1r[i]+=I1rN[i]*vacc_prov_r;
-                    deltaI1p[i]+=I1pN[i]*vacc_prov_p;
-                    deltaI1N[i]-=I1N[i]*vacc_prov;
-                    deltaI1rN[i]-=I1rN[i]*vacc_prov_r;
-                    deltaI1pN[i]-=I1pN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].i1[i]+=I1N[i]*vacc_prov;
+                    deltas[VACC_HIGH].i1[i]+=I1rN[i]*vacc_prov_r;
+                    deltas[VACC_PREG].i1[i]+=I1pN[i]*vacc_prov_p;
+                    deltas[LOW].i1[i]-=I1N[i]*vacc_prov;
+                    deltas[HIGH].i1[i]-=I1rN[i]*vacc_prov_r;
+                    deltas[PREG].i1[i]-=I1pN[i]*vacc_prov_p;
 
-                    deltaI2[i]+=I2N[i]*vacc_prov;
-                    deltaI2r[i]+=I2rN[i]*vacc_prov_r;
-                    deltaI2p[i]+=I2pN[i]*vacc_prov_p;
-                    deltaI2N[i]-=I2N[i]*vacc_prov;
-                    deltaI2rN[i]-=I2rN[i]*vacc_prov_r;
-                    deltaI2pN[i]-=I2pN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].i2[i]+=I2N[i]*vacc_prov;
+                    deltas[VACC_HIGH].i2[i]+=I2rN[i]*vacc_prov_r;
+                    deltas[VACC_PREG].i2[i]+=I2pN[i]*vacc_prov_p;
+                    deltas[LOW].i2[i]-=I2N[i]*vacc_prov;
+                    deltas[HIGH].i2[i]-=I2rN[i]*vacc_prov_r;
+                    deltas[PREG].i2[i]-=I2pN[i]*vacc_prov_p;
 
-                    deltaR[i]+=RN[i]*vacc_prov+SN[i]*vacc_prov*vaccine_programme.efficacy_year[i];
-                    deltaRr[i]+=RrN[i]*vacc_prov_r+SrN[i]*vacc_prov_r*vaccine_programme.efficacy_year[i];
-                    deltaRp[i]+=RpN[i]*vacc_prov_p+SpN[i]*vacc_prov_p*vaccine_programme.efficacy_year[i];
-                    deltaRN[i]-=RN[i]*vacc_prov;
-                    deltaRrN[i]-=RrN[i]*vacc_prov_r;
-                    deltaRpN[i]-=RpN[i]*vacc_prov_p;
+                    deltas[VACC_LOW].r[i]+=RN[i]*vacc_prov+SN[i]*vacc_prov*vaccine_programme.efficacy_year[i];
+                    deltas[VACC_HIGH].r[i]+=RrN[i]*vacc_prov_r+SrN[i]*vacc_prov_r*vaccine_programme.efficacy_year[i];
+                    deltas[VACC_PREG].r[i]+=RpN[i]*vacc_prov_p+SpN[i]*vacc_prov_p*vaccine_programme.efficacy_year[i];
+                    deltas[LOW].r[i]-=RN[i]*vacc_prov;
+                    deltas[HIGH].r[i]-=RrN[i]*vacc_prov_r;
+                    deltas[PREG].r[i]-=RpN[i]*vacc_prov_p;
                 }
             }
 
             /*update the different classes*/
             for(i=0;i<NAG;i++)
             {
-                S[i]+=h_step*deltaS[i];
-                Sr[i]+=h_step*deltaSr[i];
-                Sp[i]+=h_step*deltaSp[i];
+                S[i]+=h_step*deltas[VACC_LOW].s[i];
+                Sr[i]+=h_step*deltas[VACC_HIGH].s[i];
+                Sp[i]+=h_step*deltas[VACC_PREG].s[i];
 
-                SN[i]+=h_step*deltaSN[i];
-                SrN[i]+=h_step*deltaSrN[i];
-                SpN[i]+=h_step*deltaSpN[i];
+                SN[i]+=h_step*deltas[LOW].s[i];
+                SrN[i]+=h_step*deltas[HIGH].s[i];
+                SpN[i]+=h_step*deltas[PREG].s[i];
 
-                E1[i]+=h_step*deltaE1[i];
-                E1r[i]+=h_step*deltaE1r[i];
-                E1p[i]+=h_step*deltaE1p[i];
+                E1[i]+=h_step*deltas[VACC_LOW].e1[i];
+                E1r[i]+=h_step*deltas[VACC_HIGH].e1[i];
+                E1p[i]+=h_step*deltas[VACC_PREG].e1[i];
 
-                E1N[i]+=h_step*deltaE1N[i];
-                E1rN[i]+=h_step*deltaE1rN[i];
-                E1pN[i]+=h_step*deltaE1pN[i];
+                E1N[i]+=h_step*deltas[LOW].e1[i];
+                E1rN[i]+=h_step*deltas[HIGH].e1[i];
+                E1pN[i]+=h_step*deltas[PREG].e1[i];
 
-                E2[i]+=h_step*deltaE2[i];
-                E2r[i]+=h_step*deltaE2r[i];
-                E2p[i]+=h_step*deltaE2p[i];
+                E2[i]+=h_step*deltas[VACC_LOW].e2[i];
+                E2r[i]+=h_step*deltas[VACC_HIGH].e2[i];
+                E2p[i]+=h_step*deltas[VACC_PREG].e2[i];
 
-                E2N[i]+=h_step*deltaE2N[i];
-                E2rN[i]+=h_step*deltaE2rN[i];
-                E2pN[i]+=h_step*deltaE2pN[i];
+                E2N[i]+=h_step*deltas[LOW].e2[i];
+                E2rN[i]+=h_step*deltas[HIGH].e2[i];
+                E2pN[i]+=h_step*deltas[PREG].e2[i];
 
-                I1[i]+=h_step*deltaI1[i];
-                I1r[i]+=h_step*deltaI1r[i];
-                I1p[i]+=h_step*deltaI1p[i];
+                I1[i]+=h_step*deltas[VACC_LOW].i1[i];
+                I1r[i]+=h_step*deltas[VACC_HIGH].i1[i];
+                I1p[i]+=h_step*deltas[VACC_PREG].i1[i];
 
-                I1N[i]+=h_step*deltaI1N[i];
-                I1rN[i]+=h_step*deltaI1rN[i];
-                I1pN[i]+=h_step*deltaI1pN[i];
+                I1N[i]+=h_step*deltas[LOW].i1[i];
+                I1rN[i]+=h_step*deltas[HIGH].i1[i];
+                I1pN[i]+=h_step*deltas[PREG].i1[i];
 
-                I2[i]+=h_step*deltaI2[i];
-                I2r[i]+=h_step*deltaI2r[i];
-                I2p[i]+=h_step*deltaI2p[i];
+                I2[i]+=h_step*deltas[VACC_LOW].i2[i];
+                I2r[i]+=h_step*deltas[VACC_HIGH].i2[i];
+                I2p[i]+=h_step*deltas[VACC_PREG].i2[i];
 
-                I2N[i]+=h_step*deltaI2N[i];
-                I2rN[i]+=h_step*deltaI2rN[i];
-                I2pN[i]+=h_step*deltaI2pN[i];
+                I2N[i]+=h_step*deltas[LOW].i2[i];
+                I2rN[i]+=h_step*deltas[HIGH].i2[i];
+                I2pN[i]+=h_step*deltas[PREG].i2[i];
 
-                R[i]+=h_step*deltaR[i];
-                Rr[i]+=h_step*deltaRr[i];
-                Rp[i]+=h_step*deltaRp[i];
+                R[i]+=h_step*deltas[VACC_LOW].r[i];
+                Rr[i]+=h_step*deltas[VACC_HIGH].r[i];
+                Rp[i]+=h_step*deltas[VACC_PREG].r[i];
 
-                RN[i]+=h_step*deltaRN[i];
-                RrN[i]+=h_step*deltaRrN[i];
-                RpN[i]+=h_step*deltaRpN[i];
+                RN[i]+=h_step*deltas[LOW].r[i];
+                RrN[i]+=h_step*deltas[HIGH].r[i];
+                RpN[i]+=h_step*deltas[PREG].r[i];
 
                 total_of_new_cases_per_day[i]+=a2*(E2[i]+E2N[i])*h_step;
                 total_of_new_cases_per_day_r[i]+=a2*(E2r[i]+E2rN[i])*h_step;
