7a62dc44bb9eac32da37d4dfffaeeb8d3a0a4f73
BlackEdder <edder@tkwsping.nl>
2015-07-24 12:40:51 +0100

    autotest now uses the more reliable devtools::check()

--- fluEvidenceSynthesis/autotest.sh
+++ fluEvidenceSynthesis/autotest.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # Always run at least once
-R -e 'Rcpp::compileAttributes(".",verbose=TRUE)';
-# Should actually move needed header in inst/include/fluEvidenceSynthesis.h insted of using echo
-#echo -e "#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)" > src/RcppExports.cpp;
-R CMD check ../fluEvidenceSynthesis
+R -e 'Rcpp::compileAttributes(".",verbose=TRUE);library(devtools);devtools::check()'
 
 # This script depends on inotify-hookable
-#inotify-hookable -t 1000 -i '*.o' -i '\..*' -w data -w src -w tests -f DESCRIPTION -w R -c "R -e 'Rcpp::compileAttributes(\".\",verbose=TRUE)' && echo '#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)' > src/RcppExports.cpp && R CMD check ../fluEvidenceSynthesis; sleep 10"
-inotify-hookable -t 1000 -i '*.o' -i '\..*' -w data -w src -w tests -f DESCRIPTION -w R -c "R -e 'Rcpp::compileAttributes(\".\",verbose=TRUE)' && R CMD check ../fluEvidenceSynthesis; sleep 10"
+inotify-hookable -t 1000 -i '*.o' -i '\..*' -w data -w src -w tests -f DESCRIPTION -w R -c "R -e 'Rcpp::compileAttributes(\".\",verbose=TRUE);library(devtools);devtools::check()' && sleep 10"
