28882669a5150161141769c204f86a6c789030a8
BlackEdder <edder@tkwsping.nl>
2015-07-24 16:37:53 +0100

    runManualTests does not need to patch RcppExports any more

--- fluEvidenceSynthesis/tests/manual/runManualTests.sh
+++ fluEvidenceSynthesis/tests/manual/runManualTests.sh
@@ -3,7 +3,6 @@
 # Always run at least once
 R -e 'Rcpp::compileAttributes(".",verbose=TRUE)';
 # Should actually move needed header in inst/include/fluEvidenceSynthesis.h insted of using echo
-echo -e "#include \"rcppwrap.hh\"\n$(cat src/RcppExports.cpp)" > src/RcppExports.cpp;
 R CMD check ../fluEvidenceSynthesis
 R CMD INSTALL ../fluEvidenceSynthesis
 
