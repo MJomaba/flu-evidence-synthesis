31e710749f047aa03c1313858a202ab036757a3a
BlackEdder <edder@tkwsping.nl>
2015-08-17 16:14:22 +0100

    Add test for using different interval size than 1

--- fluEvidenceSynthesis/tests/testthat/testODE.R
+++ fluEvidenceSynthesis/tests/testthat/testODE.R
@@ -87,6 +87,45 @@ test_that("Return the correct ODE results", {
     }
 })
 
+test_that("Return the correct ODE results, when using a different interval", {
+    data("age_sizes")
+    data("vaccine_calendar")
+    data("polymod_uk")
+    data("mcmcsample")
+    odes <- infection.model( age_sizes[,1],
+                  vaccine_calendar,
+                  as.matrix(polymod_uk),
+                  mcmcsample, 7 )
+
+    # Check all sums
+    sums <- c(119002.481089555, 633877.330662713, 1463334.37622813, 2870258.66403954, 4391396.3601774, 2786119.98841863, 496487.103476314, 2531.54735113481, 36283.6707667877, 155096.824666694, 263690.71146717, 415426.823720658, 528526.506836539, 358207.414064678)
+    odeSums <- colSums(odes[,2:15])
+    for( i in 1:length(sums) )
+    {
+        ratio <- odeSums[i]/sums[i]
+        expect_less_than( ratio, 1.05 )
+        expect_more_than( ratio, 0.97 )
+    }
+
+    # Check max value
+    maxs <- c(3132.20022124067, 16720.2726361168, 38489.4726884184, 77065.0627636948, 115970.502880153, 73496.0794524567, 13004.8663674856, 66.6225150332303, 956.806903893294, 4077.66057862465, 7073.81188424645, 10955.7871500801, 13891.6328086064, 9360.94718096195)
+    odeMaxs <- sapply( seq(2,15), function(x) max(odes[,x]) ) 
+    for( i in 1:length(maxs) )
+    {
+        ratio <- odeMaxs[i]/(7*maxs[i])
+        expect_less_than( ratio, 1.03 )
+        expect_more_than( ratio, 0.97 )
+    }
+    # Check time of max
+    maxts <- c(118, 117, 118, 114, 117, 117, 118, 118, 117, 118, 114, 117, 117, 118)
+    odeMaxts <- sapply( seq(2,15), function(x) which.max(odes[,x]) )
+    for( i in 1:length(maxts) )
+    {
+        dt <- abs(odeMaxts[i]*7-maxts[i]) # Take into account different time
+        expect_less_than( dt, 6 )
+    }
+})
+
 test_that("ODE works correctly with the new vaccine date vector", {
     data("age_sizes")
     data("polymod_uk")
