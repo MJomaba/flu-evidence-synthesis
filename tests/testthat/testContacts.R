4a8993cbac5b6b4f64b932ee9445c38a8b8713aa
BlackEdder <edder@tkwsping.nl>
2015-11-03 16:22:43 +0000

    Test calls contact.matrix

--- tests/testthat/testContacts.R
+++ tests/testthat/testContacts.R
@@ -5,7 +5,7 @@ test_that("We can create a contact matrix",
       data(age_sizes)
       data(polymod_uk)
       age.group.limits <- c( 1,5,15,25,45,65 )
-      cm <- contract.matrix( as.matrix(polymod_uk), age_sizes[1,], 
+      cm <- contact.matrix( as.matrix(polymod_uk), age_sizes[1,], 
                             age.group.limits );
       expect_equal( cm[1,1], 1 )
 
