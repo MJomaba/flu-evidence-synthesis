5ab70a843b456c4d1ddfff72fc9d56916b8d0d98
BlackEdder <edder@tkwsping.nl>
2015-11-05 14:42:04 +0000

    Remove debug output

--- tests/testthat/testDemography.R
+++ tests/testthat/testDemography.R
@@ -5,7 +5,6 @@ test_that("We can separate into risk groups",
       popv <- separate.into.risk.groups( 
                 c(3000,2000,1000),
                 matrix( c(0.4,0.4,0.3,0.1,0.1,0.1), ncol=3, byrow=T ) )
-      print(popv)
       expect_identical( popv,
             c( 1500, 1000, 600, 1200, 800, 300, 300, 200, 100 ) )
 
