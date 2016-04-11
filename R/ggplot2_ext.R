8719cd1eebba9c127dbcc57814d9872de609b9e5
BlackEdder <edder@tkwsping.nl>
2016-04-11 13:08:05 +0100

    Add help for stat_ci

--- R/ggplot2_ext.R
+++ R/ggplot2_ext.R
@@ -14,6 +14,13 @@ StatCI <- ggproto("StatCI", Stat,
                   required_aes = c("x", "y","ci")
 )
 
+#' @title Draw credibility intervals
+#' 
+#' @description
+#' Will draw credibitlity intervals with different colours for each interval
+#' 
+#' @inheritParams geom_polygon
+#' 
 stat_ci <- function(mapping = NULL, data = NULL, geom = "polygon",
                        position = "identity", na.rm = FALSE, show.legend = NA, 
                        inherit.aes = TRUE, ...) {
