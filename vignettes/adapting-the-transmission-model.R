0d087fc7acdaec7ddc208ee0c0232efdf7a1eeff
BlackEdder <edder@tkwsping.nl>
2017-02-23 14:37:23 +0000

    Update all vignettes to use the new function names

--- vignettes/adapting-the-transmission-model.R
+++
@@ -1,7 +0,0 @@
-## ---- include = F, message = F, warning = F------------------------------
-chooseCRANmirror(ind = 1)
-
-## ---- eval=F-------------------------------------------------------------
-#  infectionODEs(population, initial_infected, vaccine_calendar, contact_matrix,
-#    susceptibility, transmissibility, infection_delays, interval)
-
