2aaad408e8c9f4c6d92ba8a8d092a4b75aae5854
BlackEdder <edder@tkwsping.nl>
2017-02-23 14:08:37 +0000

    Vaccination vignette should use new function names

---
+++ vignettes/adapting-the-transmission-model.R
@@ -0,0 +1,7 @@
+## ---- include = F, message = F, warning = F------------------------------
+chooseCRANmirror(ind = 1)
+
+## ---- eval=F-------------------------------------------------------------
+#  infectionODEs(population, initial_infected, vaccine_calendar, contact_matrix,
+#    susceptibility, transmissibility, infection_delays, interval)
+
