5625a3be894b03610955987df3482bc75997ca9c
BlackEdder <edder@tkwsping.nl>
2017-07-21 14:19:49 +0100

    Add age group mapping

---
+++ R/demography.R
@@ -0,0 +1,61 @@
+#' @title Calculate a mapping from one sets of age groups to another set of age groups
+#' 
+#' @description Sometimes data/results are stratified in different ways. For example in the UK data set vaccination
+#' is divided into age groups: <1, <5, <15, <25, <45, <65 and 65+, while virological data is 
+#' provided as <5, <15, <45, <65 and 65+. This function calculates a mapping from one set of age groups
+#' to another. If the age groups we want to map to is a subset of the original age groups (as in the UK)
+#' then this is straightforward. If instead one or more of the from age group needs to be split over multiple to age groups
+#' then the function weights the age group by the size of the population (demography).
+#' 
+#' @param from The upper limit to each age groups you want to map from
+#' @param to The upper limit to each age groups you want to map to
+#' @param demography Optional vector with the demography of the population. This value is needed if the to age groups are
+#' not a subset of the from age groups. First value is the population of 0 year olds.
+#' Second of 1 year olds etc...
+#' @return A data frame containing which age groups map to which other age group. And what proportion (weight)
+#' of the age groups maps to the other age grouping.
+#' @export
+age_group_mapping <- function(from, to, demography) {
+  i <- 1
+  j <- 1
+  is <- c()
+  js <- c()
+  weights <- c()
+  while(TRUE) {
+    if (j == length(to) + 1 && i == length(from) + 1) {
+      is <- c(is, i)
+      js <- c(js, j)
+      weights <- c(weights, 1)
+      break
+    } else if (j == length(to) + 1) {
+      is <- c(is, i)
+      js <- c(js, j)
+      weights <- c(weights, 1)
+      i <- i + 1
+    } else if (from[i] <= to[j]) {
+      is <- c(is, i)
+      js <- c(js, j)
+      weights <- c(weights, 1)
+      if (from[i] == to[j])
+        j <- j + 1
+      i <- i + 1
+    } else if (from[i] > to[j]) {
+      is <- c(is, i)
+      js <- c(js, j)
+      if (missing(demography))
+        stop("Mapping incompatible age groups requires demography data")
+      fa <- function(ag) if (ag < 0 || ag >= length(demography)) 0 else demography[ag+1]
+      low_w <- sum(sapply(seq(max(-1, from[i - 1], na.rm = T), to[j] - 1), fa))
+      high_w <- sum(sapply(seq(to[j], from[i] - 1), fa))
+      j <- j + 1
+      is <- c(is, i)
+      js <- c(js, j)
+      i <- i + 1
+      weights <- c(weights, low_w/(low_w+high_w), high_w/(low_w+high_w))
+      #weights <- c(weights, low_w, high_w)
+    } else {
+      warning("Invalid state in age_groups_mapping()")
+    }
+  }
+  data.frame(from = is, to = js, weight = weights)
+}
--- tests/testthat/testDemography.R
+++ tests/testthat/testDemography.R
@@ -30,4 +30,25 @@ test_that("We can convert age vector to age group", {
   v <- as_age_group(11, c(1,5,10))
   expect_equal(as.numeric(v), 4)
 })
-   
+ 
+test_that("We can map age groups", {
+  mp <- age_group_mapping(c(1,5), c(5))
+  expect_identical(as.numeric(mp$from), c(1, 2, 3))
+  expect_identical(as.numeric(mp$to), c(1, 1, 2))
+  expect_identical(mp$weight, c(1,1,1))
+  
+  mp <- age_group_mapping(c(1,5,8), c(5))
+  expect_identical(as.numeric(mp$from), c(1, 2, 3, 4))
+  expect_identical(as.numeric(mp$to), c(1, 1, 2, 2))
+  expect_identical(mp$weight, c(1, 1, 1, 1))
+  
+  mp <- age_group_mapping(c(1,3,4), c(2), demography = c(100, 200, 400, 800, 1600))
+  expect_identical(as.numeric(mp$from), c(1, 2, 2, 3, 4))
+  expect_identical(as.numeric(mp$to), c(1, 1, 2, 2, 2))
+  expect_identical(mp$weight, c(1, 200/600, 400/600, 1, 1))
+  
+  mp <- age_group_mapping(c(1,5), c(3), demography = c(100, 200, 400, 800, 1600, 3200))
+  expect_identical(as.numeric(mp$from), c(1, 2, 2, 3))
+  expect_identical(as.numeric(mp$to), c(1, 1, 2, 2))
+  expect_identical(mp$weight, c(1, 600/(600+2400), 2400/(600+2400), 1, 1))
+})
